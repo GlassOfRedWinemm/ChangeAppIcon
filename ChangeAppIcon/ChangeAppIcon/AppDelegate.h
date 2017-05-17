5c42f6d941a4bd2fde962db49324766a470661d0
iOS-Developer <iOS-Developer@iOS-TestApp.local>
2017-05-17 15:18:17 +0800

    add project

---
+++ ChangeAppIcon/ChangeAppIcon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChangeAppIcon
+//
+//  Created by iOS-Developer on 2017/5/17.
+//  Copyright © 2017年 ShengShiQuanJing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChangeAppIcon/ChangeAppIcon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChangeAppIcon
+//
+//  Created by iOS-Developer on 2017/5/17.
+//  Copyright © 2017年 ShengShiQuanJing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
